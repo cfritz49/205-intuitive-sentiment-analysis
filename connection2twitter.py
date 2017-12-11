cc4cd6bbb322993c934ee3ef3ffb3e4ffd4c007d
JL47 <jorlopez@csumb.edu>
2017-12-10 18:59:12 -0800

    hashtag function fixed

--- connection2twitter.py
+++ connection2twitter.py
@@ -1,17 +1,12 @@
-"""
-CST 205-01
+## Jorge Lopez
+## December 8, 2017
+## CST 205-01
+## Avner Biblarz
 
-Intuitive Sentiment Analysis - Using tweepy, Flask, VADER and Plot.ly to intuitively visualize social media sentiment
-analysis from Twitter
-
-Github link: https://github.com/cfritz49/205-intuitive-sentiment-analysis
-
-By Connor Fritz, Jorge Lopez, and Matthew Connolly
-
-This file pulls information from twitter with tweepy. Information can be retrieved by user, hash tag, or keyword.
-
-All work on this file by Jorge Lopez
-"""
+## Abstract: Auntheticates myself as owner.
+## Allows the user to search for users and pull their tweets.
+## I was also able to provide the user to search for a
+## specific keyword or hashtag and get those tweets
 
 import tweepy
 
@@ -22,7 +17,7 @@ acces_token = '1437064778-SMbqdOOK2Et5ftZNeEKecuIWhMS1ceFEsjnfz9P'
 acces_token_secret = 'ca58DLvuT82rlBvzpIkkkgYEEQ8md8IHJSgqbsfQsF5X5'
 
 
-# Authentication
+## Authentication
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret,'/home.html')
 
 auth.set_access_token(acces_token,acces_token_secret)
@@ -31,14 +26,20 @@ api = tweepy.API(auth)
 
     # Search hashtags along with keywords #
 def hashtag(search):
+    tweetList = []
     for tweet in tweepy.Cursor(api.search, q = search,
                                             include_entities=True,
                                             lang="en").items(10):
-        # print ("entities:", tweet.entities['hashtags'])
-        return tweet.text
+
+        if (not tweet.retweeted) and ('RT @'not in tweet.text):
+            tweetList.append(tweet)
+
+    for tweet in tweetList:
+        # print(tweet.text)
+        return tweetList
+
 
     # Accepts username and prints tweets from them #
-
 def usr(user):
     tweetcount = 10
     my_user=api.get_user(user)
@@ -47,10 +48,11 @@ def usr(user):
     print('Number of Followers:')
     print(my_user.followers_count)
     print()
-    results_2 = api.user_timeline(id=user,count=tweetcount)
+    results_2 = api.user_timeline(id=user,count=tweetcount,full_text=True)
     print("Recent tweets:")
     for tweet in results_2:
-        print(tweet.text)
+        if not tweet.retweeted:
+            print(tweet.text)
     print()
 
 # Calls usr function to search user w/ number of followers & tweets #
@@ -61,4 +63,3 @@ query = input("Enter hashtag or keyword:")
 
 
 hashtag(query)
-
