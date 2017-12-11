e0c160b30743e775a75abd75b4ab9276379efb62
cfritz49 <cfritz@csumb.edu>
2017-12-10 17:27:29 -0700

    Improved comments on analysis_plot.py

--- analysis_plot.py
+++ analysis_plot.py
@@ -6,7 +6,7 @@ Intuitive Sentiment Analysis - Analyzing and displaying positive, neutral, and n
 By Connor Fritz, Jorge Lopez, and Matthew Connolly
 
 This file takes in the list of tweets from the twitter search and analyzes them with VADER. It then uses the analysis
-data to generate a histogram of compound scores and a pie chart of average sentiment.
+data to generate a histogram of compoun d scores and a pie chart of average sentiment.
 
 All work on this file by Connor Fritz
 """
@@ -16,12 +16,15 @@ from plotly.graph_objs import *
 import vaderSentiment.vaderSentiment as vS
 
 
+# Takes in a list of tweets as strings, analyzes them, returns graphs as divs in the format (Pie chart, Histogram)
 def vader_analyze(twitter_input):
     analyzer = vS.SentimentIntensityAnalyzer()
     pos = []
     neg = []
     neu = []
     com = []
+
+    # VADER analysis
     for tweet in twitter_input:
         analyzed_tweet = analyzer.polarity_scores(tweet)
         pos.append(analyzed_tweet['pos'])
@@ -29,17 +32,20 @@ def vader_analyze(twitter_input):
         neu.append(analyzed_tweet['neu'])
         com.append(analyzed_tweet['compound'])
 
+    # Average calculation
     ave_pos = sum(pos) / float(len(pos))
     ave_neu = sum(neu) / float(len(neu))
     ave_neg = sum(neg) / float(len(neg))
 
+    # Setting up Plot.ly graphing
     labels = ['Positive', 'Neutral', 'Negative']
     values = [ave_pos, ave_neu, ave_neg]
     trace = Pie(labels=labels, values=values)
     data = [Histogram(x=com)]
 
-    divs = (plotly.offline.plot([trace], include_plotlyjs=False, output_type='div'),
-            plotly.offline.plot(data, include_plotlyjs=False, output_type='div'))
+    # Creates Plot.ly graphs and stores them in tuple as strings. The graphs are saved as HTML divs
+    divs = (plotly.offline.plot([trace], include_plotlyjs=False, output_type='div'),    # Pie Chart
+            plotly.offline.plot(data, include_plotlyjs=False, output_type='div'))       # Histogram
 
     return divs
 
